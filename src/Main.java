14ba9b30247a17053e4b24405e0f425ac9a1b39c
nyulnyul <nyul0223@naver.com>
2023-05-17 23:05:41 +0900

    인덱스 앱바 제작

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,44 @@
+
+import java.sql.*;
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    public static void main(String[] args) throws SQLException {
+        // 데이터베이스에 연결합니다.
+        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb", "root", "");
+
+        // `Statement` 개체를 만듭니다.
+        Statement stmt = con.createStatement();
+
+        // 경기 일정과 점수를 저장할 테이블을 만듭니다.
+        String sql = "CREATE TABLE games (id INT AUTO_INCREMENT PRIMARY KEY, team1 VARCHAR(255), team2 VARCHAR(255), score1 INT, score2 INT);";
+        stmt.executeUpdate(sql);
+
+        // JFrame을 만듭니다.
+        JFrame frame = new JFrame("Scoreboard");
+
+        // JPanel을 만듭니다.
+        JPanel panel = new JPanel();
+
+        // JTable을 만듭니다.
+        JTable table = new JTable();
+
+        // JTable을 데이터베이스에서 가져옵니다.
+        String sql2 = "SELECT * FROM games;";
+        ResultSet rs = stmt.executeQuery(sql2);
+        //table.setModel(new DefaultTableModel(rs.getMetaData(), rs.getRows()));
+
+        // JTable을 패널에 추가합니다.
+        panel.add(table);
+
+        // JFrame에 패널을 추가합니다.
+        frame.add(panel);
+
+        // JFrame을 화면에 표시합니다.
+        frame.pack();
+        frame.setVisible(true);
     }
 }
+
--- src/swing/index/index.java
+++ src/swing/index/index.java
@@ -1,13 +1,17 @@
 package swing.index;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 
 public class index extends javax.swing.JFrame {
+    private JLabel mainimg;
+
     public index() {
         initComponents();
     }
 
+    private  JPanel c;
     private void initComponents() {
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("메인");
@@ -16,14 +20,73 @@ public class index extends javax.swing.JFrame {
         setLayout(new BorderLayout());
         Color b=new Color(46, 64, 89);
 
-        JPanel c=new JPanel();  // 인덱스 배경화면 패널
+         c = new JPanel();  // 인덱스 배경화면 패널
         c.setBackground(b);
         add(c);
-
+        setAppbar();
         setVisible(true);
+    } //jframe 기본 세팅
+
+    private void setAppbar(){ //디폴트 메인바 세팅
+        JPanel pN = new JPanel();
+        pN.setLayout(new FlowLayout());
+
+        JToolBar toolbar = new JToolBar();
+        Color sb=new Color(76, 129, 199);
+
+        toolbar.setBackground(sb);
+        toolbar.setLayout(new GridLayout(1, 5));
+        toolbar.setBorder(new EmptyBorder(0, 5, 5, 0));
+        toolbar.setPreferredSize(new Dimension(1450, 40));
+
+        JButton mainMenu = new JButton();
+        mainMenu.setFont(new Font("맑은 고딕", Font.PLAIN, 20));
+        mainMenu.setBorderPainted(false);
+        toolbar.add(mainMenu);
+        JButton teamMenu = new JButton("팀명단");
+        teamMenu.setFont(new Font("맑은 고딕", Font.PLAIN, 20));
+        teamMenu.setBorderPainted(false);
+        toolbar.add(teamMenu);
+        JButton BEMenu = new JButton("베스트일레븐");
+        BEMenu.setFont(new Font("맑은 고딕", Font.PLAIN, 20));
+        BEMenu.setBorderPainted(false);
+        toolbar.add(BEMenu);
+        JButton toMenu = new JButton("토너먼트");
+        toMenu.setFont(new Font("맑은 고딕", Font.PLAIN, 20));
+        toMenu.setBorderPainted(false);
+        toolbar.add(toMenu);
+        JButton statMenu = new JButton("스텟");
+        statMenu.setFont(new Font("맑은 고딕", Font.PLAIN, 20));
+        statMenu.setBorderPainted(false);
+        toolbar.add(statMenu);
+
+
+        JLabel imgLabel = new JLabel();
+        ImageIcon icon = new ImageIcon("/Users/nyul/Desktop/Footproject/index/src/swing/index/image/mmain.png");
+        Image img = icon.getImage();
+        Image updateImg = img.getScaledInstance(200, 40, Image.SCALE_SMOOTH);
+        ImageIcon updateIcon = new ImageIcon(updateImg);
+        imgLabel.setIcon(updateIcon);
+        imgLabel.setBounds(100, 30, 265, 150);
+        imgLabel.setHorizontalAlignment(JLabel.CENTER);
+        mainMenu.add(imgLabel);
+
+        JTextField serchbar = new JTextField(5);
+        serchbar.setPreferredSize(new Dimension(500, 20));
+        toolbar.add(serchbar);
+        JButton btn5 = new JButton("검색");
+        btn5.setPreferredSize(new Dimension(150, 20));
+
+        toolbar.add(btn5);
+        Color b=new Color(46, 64, 89);
+        pN.setBackground(b);
+        pN.add(toolbar, BorderLayout.NORTH);
+        add(pN, BorderLayout.NORTH);
     }
 
     public static void main(String[] args) {
+
         new index();
+
     }
 }
